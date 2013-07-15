d47ecdd922002201edf6628044ed1d8da9c6333d
snake <elserpiente@rambler.ru>
2013-07-15 14:43:36 +0300

    added initial files

---
+++ MySimpleRobot.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+class MySimpleRobot:
+	"""a realization of my robot"""
+	def __init__(self):
+		self.x = 0
+		self.y = 0
+		self.angle = 0
+	
+
+
+def main():
+	r = MySimpleRobot();
+	
+	return 0
+
+if __name__ == '__main__':
+	main()
+
---
+++ MySimpleSDLRobot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#sys.path.append('/')
+from MySimpleRobot import *
+
+class MySimpleSDLRobot(MySimpleRobot):
+	pass;
+
+
+def main():
+	r = MySimpleSDLRobot();
+	return 0
+
+if __name__ == '__main__':
+	main()
+
