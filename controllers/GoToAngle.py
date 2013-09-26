0283d29814b639a8c385bbfc07e4d0593fd7602e
snake <elserpiente@rambler.ru>
2013-09-26 12:59:01 +0300

    added controllers

---
+++ controllers/GoToAngle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from math import *
+
+class GoToAngle(object):
+	def __init__(self, Kp=10):
+		self.Kp = Kp
+	def execute(self, robot, inputs, outputs, dt):
+		x, y, angle = robot.get_position()
+		v = inputs.v
+		angle_d = inputs.angle_d
+
+		e_k = radians(angle_d - angle)
+		e_k = degrees(atan2(sin(e_k), cos(e_k)))
+		w = self.Kp*e_k
+		outputs.v = v
+		outputs.w = w
+		print('v = ', v)
+		print('w = ', w)
+
+
+		
+
---
+++ controllers/GoToGoal.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+class GoToGoal(object):
+	def execute(self, robot):
+		x, y, theta = robot.get_data()
+		
+
